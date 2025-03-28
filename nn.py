e00b3503778cceb49d30c44580fadd118a3fcb46
Parvfect <parvagrw02@gmail.com>
2025-03-28 09:39:43 +0000

    Removing downsampling

--- nn.py
+++ nn.py
@@ -68,13 +68,13 @@ class NaiveCaller(nn.Module):
         
         # Convolutional feature extractor
         self.cnn = nn.Sequential(
-            nn.Conv1d(input_dim, 32, kernel_size=5, stride=1, dilation=1),  
+            nn.Conv1d(input_dim, 32, kernel_size=3, stride=1, dilation=1),  
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=5, stride=3),
-            nn.Conv1d(32, 64, kernel_size=5, stride=1, dilation=2),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(32, 64, kernel_size=3, stride=1, dilation=2),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=5, stride=3),
-            nn.Conv1d(64, 128, kernel_size=7, stride=2, dilation=2),
+            #nn.MaxPool1d(kernel_size=5, stride=5),
+            nn.Conv1d(64, 128, kernel_size=5, stride=2, dilation=2),
             nn.ReLU(),
             nn.Conv1d(128, conv_out, kernel_size=5, stride=1, dilation=4),
             nn.ReLU()
