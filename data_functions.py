2866f26b65e983f0b6c1ae29cb197e576e657632
Parvfect <parvagrw02@gmail.com>
2025-05-27 15:31:23 +0100

    Data prepping and cleaning and some evaluation

--- data_functions.py
+++ data_functions.py
@@ -3,29 +3,83 @@ from ont_fast5_api.fast5_interface import get_fast5_file
 import pandas as pd
 
 
-def get_data_from_fast5(fast5_filepath: str):
+def get_data_from_fast5(fast5_filepath: str, read_ids_db=None):
     raw_data_arr = []
     read_ids = []
     with get_fast5_file(fast5_filepath, mode="r") as f5:
         for read in f5.get_reads():
+            if read_ids_db:  # Extracting only those reads that are within the read_ids
+                if not read.read_id in read_ids_db:
+                    continue
             raw_data = read.get_raw_data()
             raw_data_arr.append(raw_data)
             read_ids.append(read.read_id)
     return raw_data_arr, read_ids
 
 
-def get_cleaned_encoded_file(encoded_df):
+def get_cleaned_encoded_file(encoded_df, address=False):
 
     # Joining payloads
     payload_cols = [col for col in encoded_df.columns if col.startswith('Payload')]
     encoded_df['payload'] = encoded_df[payload_cols].astype(str).agg(', '.join, axis=1)
+    encoded_df['payload'] = encoded_df['payload'].apply(lambda x: list(eval(x)))
 
-    # Fixing addresses
-    encoded_df['Address_Incrementer_1'] = encoded_df['Address_Incrementer_1'].apply(lambda x: f'barcode_external0{x[1]}')
-    encoded_df['Address_Incrementer_2'] = encoded_df['Address_Incrementer_2'].apply(lambda x: f'_internal0{x[1]}')
-    address_cols = [col for col in encoded_df.columns if col.startswith('Address')]
-    encoded_df['HW_Address'] = encoded_df[address_cols].astype(str).agg(''.join, axis=1)
+    if address:
+        # Fixing addresses
+        encoded_df['Address_Incrementer_1'] = encoded_df['Address_Incrementer_1'].apply(lambda x: f'barcode_external0{x[1]}')
+        encoded_df['Address_Incrementer_2'] = encoded_df['Address_Incrementer_2'].apply(lambda x: f'_internal0{x[1]}')
+        address_cols = [col for col in encoded_df.columns if col.startswith('Address')]
+        encoded_df['HW_Address'] = encoded_df[address_cols].astype(str).agg(''.join, axis=1)
 
-    # Selecting important columns
-    encoded_df = encoded_df[['HW_Address', 'payload']]
+        # Selecting important columns
+        encoded_df = encoded_df[['HW_Address', 'payload']]
     return encoded_df
+
+
+def library_motif_to_sequence(library_motif_preds):
+    """Converts motif search predictions to sorted sequences"""
+    return
+
+
+def create_spacer_sequence(payload_prediction):
+    """Returns spacer sequence from the payload cycle level prediction (for training)"""
+    return
+
+def sort_transcript(transcript):
+    """Create payload level prediction from the transcript """
+
+    cycles = [[] for i in range(8)]
+    
+    if type(transcript) == str:
+        transcript = transcript.split()
+    
+    split_transcript = [int(i) for i in transcript if i != '']
+    
+    for i in range(len(split_transcript)):
+
+        found_motif = split_transcript[i]
+
+        # If we have a payload motif
+        if found_motif < 9:
+
+            # finding the spacers - only for payload cycles
+            if i > 0:
+                # Checking for Back Spacer
+                if split_transcript[i-1] > 10:
+                    cycle_number = split_transcript[i-1] - 11
+                    cycles[cycle_number].append(split_transcript[i])
+
+                # Checking for Forward Spacer
+                elif i < len(split_transcript) - 1:
+                    if split_transcript[i+1] > 10:
+                        cycle_number = split_transcript[i+1] - 11
+                        cycles[cycle_number].append(split_transcript[i])
+
+            else:
+                if i < len(split_transcript) - 1:
+                    # Checking for Forward Spacer
+                    if split_transcript[i+1] > 10:
+                        cycle_number = split_transcript[i+1] - 11
+                        cycles[cycle_number].append(split_transcript[i])   
+    return [list(set(i)) for i in cycles]
+
